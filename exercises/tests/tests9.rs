e4ad73286acb0571155b99a2351f520bdba28a81
Xiao Jiong <jiongxnext0809@gmail.com>
2023-10-17 15:12:51 +0800

    cherrs!!!

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,16 +27,21 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-// I AM NOT DONE
 
 extern "Rust" {
     fn my_demo_function(a: u32) -> u32;
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
-mod Foo {
+mod foo {
     // No `extern` equals `extern "Rust"`.
-    fn my_demo_function(a: u32) -> u32 {
+    #[no_mangle]
+    pub fn my_demo_function(a: u32) -> u32 {
+        a
+    }
+
+    #[no_mangle]
+    pub fn my_demo_function_alias(a: u32) ->u32 {
         a
     }
 }
